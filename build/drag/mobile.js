7dd700b9fc8e73eeb97eacb01d48914e18edccf5
Richard Zille <therichkid@gmx.de>
2021-12-08 13:31:09 +0100

    Only register mobile event handlers on touch start

--- build/drag/mobile.js
+++ build/drag/mobile.js
@@ -8,22 +8,26 @@ export const addMobileDragHandlers = (tile) => {
         const startTile = event.target;
         const { pageX: startX, pageY: startY } = event.changedTouches[0];
         setDragTile(startTile, [startX, startY]);
+        window.addEventListener("touchmove", onTouchMove);
+        window.addEventListener("touchend", onTouchEnd);
         event.preventDefault();
     };
-    tile.ontouchmove = throttle((event) => {
-        const { pageX: currX, pageY: currY } = event.changedTouches[0];
-        moveDragTile([currX, currY]);
-    }, 20);
-    tile.ontouchend = (event) => {
-        toggleTileGrid(false);
-        const { pageX: endX, pageY: endY } = event.changedTouches[0];
-        const startTile = refs.dragTile;
-        const [, endTile] = document.elementsFromPoint(endX, endY);
-        unsetDragTile();
-        if (!wasDroppedOnTile(endTile)) {
-            return;
-        }
-        swapNodes(startTile, endTile);
-        setTimeout(evaluateTileOrder);
-    };
+};
+const onTouchMove = throttle((event) => {
+    const { pageX: currX, pageY: currY } = event.changedTouches[0];
+    moveDragTile([currX, currY]);
+}, 20);
+const onTouchEnd = (event) => {
+    window.removeEventListener("touchmove", onTouchMove);
+    window.removeEventListener("touchend", onTouchEnd);
+    toggleTileGrid(false);
+    const { pageX: endX, pageY: endY } = event.changedTouches[0];
+    const startTile = refs.dragTile;
+    const [, endTile] = document.elementsFromPoint(endX, endY);
+    unsetDragTile();
+    if (!wasDroppedOnTile(endTile)) {
+        return;
+    }
+    swapNodes(startTile, endTile);
+    setTimeout(evaluateTileOrder);
 };
--- src/drag/mobile.ts
+++ src/drag/mobile.ts
@@ -18,29 +18,35 @@ export const addMobileDragHandlers = (tile: HTMLDivElement): void => {
 		const { pageX: startX, pageY: startY } = event.changedTouches[0];
 		setDragTile(startTile, [startX, startY]);
 
+		window.addEventListener("touchmove", onTouchMove);
+		window.addEventListener("touchend", onTouchEnd);
+
 		event.preventDefault();
 	};
+};
 
-	tile.ontouchmove = throttle((event: TouchEvent): void => {
-		const { pageX: currX, pageY: currY } = event.changedTouches[0];
-		moveDragTile([currX, currY]);
-	}, 20);
+const onTouchMove = throttle((event: TouchEvent): void => {
+	const { pageX: currX, pageY: currY } = event.changedTouches[0];
+	moveDragTile([currX, currY]);
+}, 20);
 
-	tile.ontouchend = (event: TouchEvent): void => {
-		toggleTileGrid(false);
+const onTouchEnd = (event: TouchEvent): void => {
+	window.removeEventListener("touchmove", onTouchMove);
+	window.removeEventListener("touchend", onTouchEnd);
 
-		const { pageX: endX, pageY: endY } = event.changedTouches[0];
-		const startTile = <HTMLDivElement>refs.dragTile;
-		const [, endTile] = <HTMLDivElement[]>document.elementsFromPoint(endX, endY);
+	toggleTileGrid(false);
 
-		unsetDragTile();
+	const { pageX: endX, pageY: endY } = event.changedTouches[0];
+	const startTile = <HTMLDivElement>refs.dragTile;
+	const [, endTile] = <HTMLDivElement[]>document.elementsFromPoint(endX, endY);
 
-		if (!wasDroppedOnTile(endTile)) {
-			return;
-		}
+	unsetDragTile();
 
-		swapNodes(startTile, endTile);
+	if (!wasDroppedOnTile(endTile)) {
+		return;
+	}
 
-		setTimeout(evaluateTileOrder);
-	};
+	swapNodes(startTile, endTile);
+
+	setTimeout(evaluateTileOrder);
 };
